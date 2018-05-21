c3e4c43c0a51fa6f243641a5b548f9f5141c8681
mering <canhuayin@gmail.com>
2018-05-21 20:11:49 +0800

    更新middleware

--- Middlewares/middleware.py
+++ Middlewares/middleware.py
@@ -7,24 +7,13 @@ from django.http.multipartparser import MultiPartParser
 
 class MethodConvertMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        method = request.method
+        method = request.method.upper()
+        if 'application/json' in request.META['CONTENT_TYPE']:
+            data = json.loads(request.body.decode())
+        elif 'multipart/form-data' in request.META['CONTENT_TYPE']:
+            data, files = MultiPartParser(
+                request.META, request, request.upload_handlers).parse()
+            setattr(request, method.upper()+'_FILES', files)
+        setattr(request, method, data)
         if 'HTTP_X_METHOD' in request.META:
-            method = request.META['HTTP_X_METHOD'].upper()
-            if 'application/json' in request.META['CONTENT_TYPE']:
-                data = json.loads(request.body.decode())
-            elif 'multipart/form-data' in request.META['CONTENT_TYPE']:
-                data = request.POST
-                files = request.FILES
-                setattr(request, method.upper()+'_FILES', files)
             setattr(request, 'method', method)
-            setattr(request, method.upper(), data)
-        else:
-            if method in ['PUT', 'DELETE', 'HEAD', 'OPTIONS']:
-                if 'application/json' in request.META['CONTENT_TYPE']:
-                    POST = json.loads(request.body.decode())
-                    setattr(request, method.upper(), POST)
-                elif 'multipart/form-data' in request.META['CONTENT_TYPE']:
-                    data, files = MultiPartParser(
-                        request.META, request, request.upload_handlers).parse()
-                    setattr(request, method.upper()+'_FILES', files)
-                    setattr(request, method.upper(), data)
