4b7074659b3dd0656303085a384a725bd2301fed
mering <canhuayin@gmail.com>
2018-05-21 16:28:18 +0800

    更新中间件

--- Middlewares/middleware.py
+++ Middlewares/middleware.py
@@ -10,16 +10,21 @@ class MethodConvertMiddleware(MiddlewareMixin):
         method = request.method
         if 'HTTP_X_METHOD' in request.META:
             method = request.META['HTTP_X_METHOD'].upper()
+            if 'application/json' in request.META['CONTENT_TYPE']:
+                data = json.loads(request.body.decode())
+            elif 'multipart/form-data' in request.META['CONTENT_TYPE']:
+                data = request.POST
+                files = request.FILES
+                setattr(request, method.upper()+'_FILES', files)
+            setattr(request, 'method', method)
+            setattr(request, method.upper(), data)
         else:
             if method in ['PUT', 'DELETE', 'HEAD', 'OPTIONS']:
                 if 'application/json' in request.META['CONTENT_TYPE']:
                     POST = json.loads(request.body.decode())
                     setattr(request, method.upper(), POST)
                 elif 'multipart/form-data' in request.META['CONTENT_TYPE']:
-                    POST, FILES = MultiPartParser(
+                    data, files = MultiPartParser(
                         request.META, request, request.upload_handlers).parse()
-                    setattr(request, method.upper()+'_FILES', FILES)
-                    setattr(request, method.upper(), POST)
-                else:
-                    setattr(request, method.upper(), {})
-
+                    setattr(request, method.upper()+'_FILES', files)
+                    setattr(request, method.upper(), data)
