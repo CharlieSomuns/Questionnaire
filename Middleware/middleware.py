3e56930e1028f7d9a239a64d3b27357fa647520c
mering <canhuayin@gmail.com>
2018-05-21 11:15:43 +0800

    删除不用的中间件

--- Middleware/middleware.py
+++ Middleware/middleware.py
@@ -5,14 +5,6 @@ from django.utils.deprecation import MiddlewareMixin
 from django.http.multipartparser import MultiPartParser
 
 
-class CommonMiddleware(MiddlewareMixin):
-    def process_request(self, request,):
-        pass
-
-    def process_response(self, request, response):
-        pass
-
-
 class MethodConvertMiddleware(MiddlewareMixin):
     def process_request(self, request):
         method = request.method
