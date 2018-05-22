203b080fff72c08247cb31a53f91320a514ce7d4
mering <canhuayin@gmail.com>
2018-05-22 13:04:34 +0800

    更新问题接口

--- Api/view.py
+++ Api/view.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_exempt
 from django.db.transaction import atomic
+from django.db.models import Q
 
 from Question.models import *
 from Api.resources import Resource
@@ -218,7 +219,73 @@ class QuestionnaireResource(Resource):
 
     @customer_required
     def get(self, request, *args, **kwargs):
-        return json_response({})
+        data = request.GET
+        state = data.get('state', False)
+        limit = data.get('limit', 15)
+        start_id = data.get('start_id', False)
+        title = data.get('title', False)
+        create_time = data.get('create_time', False)
+
+        Qs = []
+        if state:
+            state = [int(state)]
+        else:
+            state = [0, 1, 2, 3, 4]
+        Qs.append(Q(state__in=state))
+
+        if start_id:
+            start_id = int(start_id)
+        else:
+            start_id = 0
+        Qs.append(Q(id__gt=start_id))
+
+        if title:
+            Qs.append(Q(title__contains=title))
+
+        if create_time:
+            create_time = datetime.strptime(create_time, '%Y-%m-%d')
+            Qs.append(Q(datetime__gt=create_time))
+
+        Qs.append(Q(customer=request.user.customer))
+
+        objs = Questionnaire.objects.filter(*Qs)
+
+        data = []
+        for obj in objs:
+            # 构建单个问卷信息
+            obj_dict = dict()
+            obj_dict['id'] = obj.id
+            obj_dict['title'] = obj.title
+            obj_dict['logo'] = obj.logo
+            obj_dict['datetime'] = datetime.strftime(obj.datetime, "%Y-%m-%d")
+            obj_dict['deadline'] = datetime.strftime(obj.deadline, "%Y-%m-%d")
+            obj_dict['catogory'] = obj.catogory
+            obj_dict['state'] = obj.state
+            obj_dict['quantity'] = obj.quantity
+            obj_dict['background'] = obj.background
+            # 读取全部标签
+            obj_dict['marks'] = [{'id': mark.id, 'name': mark.name,
+                                  'description': mark.description}for mark in obj.marks.all()]
+
+            # 构建问卷下的问题
+            obj_dict['questions'] = []
+            for question in obj.question_set.all():
+                # 构建单个问题
+                question_dict = dict()
+                question_dict['id'] = question.id
+                question_dict['title'] = question.title
+                question_dict['is_checkbox'] = question.is_checkbox
+                # 构建问题选项
+                question_dict['items'] = [{
+                    "id": item.id,
+                    "content": item.content
+                } for item in question.questionitem_set.all()]
+                # 将问题添加到问卷的问题列表中
+                obj_dict['questions'].append(question_dict)
+            # 将问卷添加到问卷列表中
+            data.append(obj_dict)
+
+        return json_response(data)
 
     @atomic
     @customer_required
@@ -316,6 +383,62 @@ class QuestionnaireResource(Resource):
             "questionnaire": '更新成功'
         })
 
+    @customer_required
+    def delete(self, request, *args, **kwargs):
+        data = request.DELETE
+        ids = data.get('ids', [])
+        objs = Questionnaire.objects.filter(
+            id__in=ids, customer=request.user.customer, state__in=[0, 1, 2, 3])
+        deleted_ids = [obj.id for obj in objs]
+        objs.delete()
+        return json_response({
+            'deleted_ids': deleted_ids
+        })
+
+
+class QuestionResource(Resource):
+    @atomic
+    @customer_required
+    def put(self, request, *args, **kwargs):
+        data = request.PUT
+        questionnaire_id = data.get('questionnaire_id', 0)
+        questionnaire_exists = Questionnaire.objects.filter(id=questionnaire_id,
+                                                            customer=request.user.customer, state__in=[0, 2, 3])
+        if questionnaire_exists:
+            questionnaire = questionnaire_exists[0]
+        else:
+            return params_error({
+                'questionnaire_id': '找不到问卷,或者问卷不可修改'
+            })
+        # 添加问题
+        question = Question()
+        question.questionnaire = questionnaire
+        question.title = data.get('title', '题纲')
+        question.is_checkbox = data.get('is_checkbox', False)
+        question.save()
+        # 修改问卷状态
+        questionnaire.state = 0
+        questionnaire.save()
+        # 添加问题选项
+        # items=['aaaa','bbbb','cccc','ddddd']
+        items = data.get('items', [])
+
+        for item in items:
+            question_item=QuestionItem()
+            question_item.question=question
+            question_item.content=item
+            question_item.save()
+
+        return json_response({
+            'id': question.id
+        })
+
+    @atomic
+    @customer_required
+    def post(self, request, *args, **kwargs):
+        return json_response({})
+
+    @atomic
     @customer_required
     def delete(self, request, *args, **kwargs):
         return json_response({})
