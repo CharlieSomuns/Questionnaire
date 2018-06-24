1413de5c1ee80501893756e5fc63d1c49bfe9043
mering <canhuayin@gmail.com>
2018-06-24 20:16:45 +0800

    修该文件夹名称

--- QuestionNaire/settings.py
+++ QuestionNaire/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'Questionnaire.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'questionnaire',
+        'NAME': 'questionnaire2',
         'USER': 'django',
         'PASSWORD': 'djangopwd',
         'HOST': 'localhost',
